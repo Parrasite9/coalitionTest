c489bc8a4364a6f2ede0d39bcf1a0c68edd7ca12
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-10-14 13:48:00 -0500

    MiddleNav completed, Hella ugly though.

--- src/Components/MiddleNav.js
+++ src/Components/MiddleNav.js
@@ -6,13 +6,13 @@ function MiddleNav() {
     <div className='middleNav'>
       <div className="logo__section">
         <img src="images/logo1.png" alt="logo" />
-        <h3>LosAngeles</h3>
+        <h3 className='firstName'>LosAngeles</h3>
         <h3>Mountains</h3>
       </div>
 
       <div className="middleNav__navlinks">
-        <div className="navlinks"><a href="#">01. History</a> </div>
-        <div className="navlinks"><a href="#">02. Team</a> </div>
+        <a href="#">01. History</a>
+        <a href="#">02. Team</a>
       </div>
     </div>
   )
