94dd7c1137fb2a436731d424b8f97fb5be864bea
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-10-13 20:08:54 -0500

    added images

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className="logo">
+        {/* INSERT LOGO HERE  */}
+      </div>
+
+      <div className="navlink__section">
+        <div className="navlink">01. HISTORY</div>
+        <div className="navlink">02. TEAM</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
