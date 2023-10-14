e28a972ed4ceef9ea24154138713313c37728502
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-10-13 20:47:12 -0500

    Image has been added to the background and image has been placed behind navbar as well.

---
+++ src/Components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../CSS/Hero.css'
+
+function Hero() {
+  return (
+    <div className='hero'>
+      <div className="hero__Text">
+        <h1><span className='hero__Text1'>LosAngeles</span></h1>
+        <h1><span className='hero__Text2'>Mountains</span></h1>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
