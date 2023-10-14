876ef34420eeea887feb314bc6b996d20c81bcc9
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-10-13 21:28:34 -0500

    Hero section completed

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import '../CSS/Hero.css'
+// import React from 'react'
+// import '../CSS/Hero.css'
+
+// function Hero() {
+//   return (
+//     <div className='hero'>
+//       <div className="hero__Text">
+//         <h1><span className='hero__Text1'>LosAngeles</span></h1>
+//         <h1><span className='hero__Text2'>Mountains</span></h1>
+//       </div>
+
+//       <div className="overlay"></div>
+//     </div>
+//   )
+// }
+
+// export default Hero
+
+import React from 'react';
+import '../CSS/Hero.css';
 
 function Hero() {
   return (
     <div className='hero'>
-      <div className="hero__Text">
+      <div className="background-image"></div> {/* Original background image */}
+      <div className="overlay"></div> {/* Overlay */}
+      <div className="hero__Text"> {/* Content */}
         <h1><span className='hero__Text1'>LosAngeles</span></h1>
         <h1><span className='hero__Text2'>Mountains</span></h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
